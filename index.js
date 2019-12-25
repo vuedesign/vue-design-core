0f2acacee6b37ecc0e7a33a39774f6146645ea00
n.see <gooarter@gmail.com>
2019-12-26 01:46:15 +0800

    develop init

---
+++ index.js
@@ -0,0 +1,21 @@
+import VueDesign, {
+    router,
+    store,
+    http,
+    to,
+    filtersCommit,
+    App,
+    UIComponents
+} from './lib/vueDesign';
+
+export {
+    router,
+    store,
+    http,
+    to,
+    filtersCommit,
+    App,
+    UIComponents
+}
+
+export default VueDesign;
